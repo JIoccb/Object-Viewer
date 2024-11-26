ebd98d9b83c3206c6aeec26de9ae53fd86115129
Artyom Rashevsky <albatroz3000@yandex.ru>
2024-11-27 02:04:17 +0300

    Use our library for linear algebra, add TODOs

--- src/main/java/com/cgvsu/render_engine/RenderEngine.java
+++ src/main/java/com/cgvsu/render_engine/RenderEngine.java
@@ -3,29 +3,31 @@ package com.cgvsu.render_engine;
 import java.util.ArrayList;
 
 import com.cgvsu.math.matrices.Matrix4D;
+import com.cgvsu.math.operations.BinaryOperations;
 import com.cgvsu.math.vectors.Vector2D;
 import com.cgvsu.math.vectors.Vector3D;
 import javafx.scene.canvas.GraphicsContext;
 
 import com.cgvsu.model.Model;
+
 import static com.cgvsu.render_engine.GraphicConveyor.*;
 
 public class RenderEngine {
-
+    //TODO починить типы данных
     public static void render(
             final GraphicsContext graphicsContext,
             final Camera camera,
             final Model mesh,
             final int width,
-            final int height)
-    {
-        Matrix4D modelMatrix = rotateScaleTranslate();
+            final int height) throws Exception {
+        Matrix4D modelMatrix = rotateScaleTranslate(); //TODO задать параметры
         Matrix4D viewMatrix = camera.getViewMatrix();
         Matrix4D projectionMatrix = camera.getProjectionMatrix();
 
-        Matrix4D modelViewProjectionMatrix = new Matrix4D(modelMatrix);
-        modelViewProjectionMatrix.mul(viewMatrix);
-        modelViewProjectionMatrix.mul(projectionMatrix);
+        Matrix4D modelViewProjectionMatrix = modelMatrix;
+
+        modelViewProjectionMatrix = BinaryOperations.product(projectionMatrix, BinaryOperations.product(viewMatrix,
+                modelViewProjectionMatrix));
 
         final int nPolygons = mesh.polygons.size();
         for (int polygonInd = 0; polygonInd < nPolygons; ++polygonInd) {
@@ -38,10 +40,10 @@ public class RenderEngine {
 
                 Vector3D vertexMath = new Vector3D(new double[]{vertex.get(0), vertex.get(1), vertex.get(2)});
 
-                Vector2D resultPoint = vertexToPoint(multiplyMatrix4ByVector3(modelViewProjectionMatrix, vertexMath), width, height);
+                Vector2D resultPoint = vertexToPoint(BinaryOperations.product(modelViewProjectionMatrix, vertexMath.increaseDimension()), width, height);
                 resultPoints.add(resultPoint);
             }
-
+            //TODO разобраться с кодом ниже
             for (int vertexInPolygonInd = 1; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
                 graphicsContext.strokeLine(
                         resultPoints.get(vertexInPolygonInd - 1).x,
