35b990e2eb06ff62870e39aff9ae2b8fac554a78
Artyom Rashevsky <albatroz3000@yandex.ru>
2024-11-29 10:42:36 +0300

    chore: add 'junit'

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.cgvsu {
     requires javafx.controls;
     requires javafx.fxml;
-    requires vecmath;
     requires java.desktop;
+    requires junit;
 
 
     opens com.cgvsu to javafx.fxml;
