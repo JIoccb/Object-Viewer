8c62c3087901c50f8f8bc1504cb13a5320a61dc1
Artyom Rashevsky <albatroz3000@yandex.ru>
2024-12-08 23:46:45 +0300

    refactor: move 'rotateScaleTranslate' to 'AffineTransformations'

--- src/main/java/com/cgvsu/math/affine_transformations/AffineTransformations.java
+++ src/main/java/com/cgvsu/math/affine_transformations/AffineTransformations.java
@@ -1,6 +1,8 @@
 package com.cgvsu.math.affine_transformations;
 
 import com.cgvsu.math.matrices.Matrix4D;
+import com.cgvsu.math.operations.BinaryOperations;
+import com.cgvsu.math.vectors.Vector3D;
 
 public class AffineTransformations {
     public static Matrix4D rotation(double x, double y, double z) {
@@ -30,5 +32,9 @@ public class AffineTransformations {
                 {0, 0, 1, -z},
                 {0, 0, 0, 1},
         });
+    }
+    public static Matrix4D rotateScaleTranslate(Vector3D translate, double alpha, double beta, double gamma, double x, double y, double z) {
+        Matrix4D RS = BinaryOperations.product(scaling(x, y, z), rotation(alpha, beta, gamma));
+        return BinaryOperations.product(translation(translate.get(0), translate.get(1), translate.get(2)), RS);
     }
 }
--- src/main/java/com/cgvsu/render_engine/GraphicConveyor.java
+++ src/main/java/com/cgvsu/render_engine/GraphicConveyor.java
@@ -6,14 +6,7 @@ import com.cgvsu.math.operations.BinaryOperations;
 import com.cgvsu.math.vectors.Vector2D;
 import com.cgvsu.math.vectors.Vector3D;
 
-import static com.cgvsu.math.affine_transformations.AffineTransformations.*;
-
 public class GraphicConveyor {
-
-    public static Matrix4D rotateScaleTranslate(Vector3D translate, double alpha, double beta, double gamma, double x, double y, double z) {
-        Matrix4D RS = BinaryOperations.product(scaling(x, y, z), rotation(alpha, beta, gamma));
-        return BinaryOperations.product(translation(translate.get(0), translate.get(1), translate.get(2)), RS);
-    }
 
     public static Matrix4D lookAt(Vector3D eye, Vector3D target) throws Exception {
         return lookAt(eye, target, new Vector3D(new double[]{0, -1, 0}));
@@ -75,18 +68,10 @@ public class GraphicConveyor {
         return result;
     }
 
-
-    /*public static Vector3D multiplyMatrix4ByVector3(final Matrix4D matrix, final Vector3D vertex) {
-        final float x = (vertex.x * matrix.m00) + (vertex.y * matrix.m10) + (vertex.z * matrix.m20) + matrix.m30;
-        final float y = (vertex.x * matrix.m01) + (vertex.y * matrix.m11) + (vertex.z * matrix.m21) + matrix.m31;
-        final float z = (vertex.x * matrix.m02) + (vertex.y * matrix.m12) + (vertex.z * matrix.m22) + matrix.m32;
-        final float w = (vertex.x * matrix.m03) + (vertex.y * matrix.m13) + (vertex.z * matrix.m23) + matrix.m33;
-        return new Vector3D(x / w, y / w, z / w);
-    }*/
-
     /*public static Vector2D vertexToPoint(final Vector3D vertex, final int width, final int height) {
         return new Vector2D(new double[]{vertex.get(0) * width + width / 2.0F, -vertex.get(1) * height + height / 2.0F});
     }*/
+
     public static Vector2D vertexToPoint(final Vector3D vertex, final int width, final int height) {
         return new Vector2D(new double[]{(width - 1) / 2.0D * (vertex.get(0) + 1), (height - 1) / 2.0D * (-vertex.get(1) + 1)});
     }
