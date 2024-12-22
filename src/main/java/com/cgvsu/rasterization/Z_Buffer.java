de95458e12f6918bef62ea2a50914a0bb34aad54
Nikolay <nikolayisaev258@gmail.com>
2024-12-22 20:53:31 +0300

    refactor

--- src/main/java/com/cgvsu/rasterization/Z_Buffer.java
+++ src/main/java/com/cgvsu/rasterization/Z_Buffer.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 public class Z_Buffer {
     private int width;
     private int height;
-    private double z;
-    private double[][] zBuffer = new double[width][height];
+
+    private final double[][] zBuffer = new double[width][height];
 
     public Z_Buffer(int width, int height) {
         this.width = width;
         this.height = height;
-        double value = Double.MAX_VALUE;
 
         for (int i = 0; i < width; i++){ // заполнения буфера одинаковыми большими значениями
-            Arrays.fill(zBuffer[i], value);
+            Arrays.fill(zBuffer[i], Double.MAX_VALUE);
         }
     }
     public void set(int x, int y, double z){
