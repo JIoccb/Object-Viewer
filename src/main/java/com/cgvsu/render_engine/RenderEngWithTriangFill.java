a7b64e69920c1067c53bf6fb169dc42ec184670c
Nikolay <nikolayisaev258@gmail.com>
2024-12-28 16:12:00 +0300

    Add call methods for rasterization with one color and rasterization with overlay texture

--- src/main/java/com/cgvsu/render_engine/RenderEngWithTriangFill.java
+++ src/main/java/com/cgvsu/render_engine/RenderEngWithTriangFill.java
@@ -7,10 +7,14 @@ import com.cgvsu.math.vectors.Vector2D;
 import com.cgvsu.math.vectors.Vector3D;
 import com.cgvsu.math.vectors.Vector4D;
 import com.cgvsu.model.Model;
+import com.cgvsu.rasterization.RasterezationOneColor;
+import com.cgvsu.rasterization.Rasterization;
 import com.cgvsu.rasterization.Z_Buffer;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
@@ -24,6 +28,11 @@ public class RenderEngWithTriangFill {
             final Model mesh,
             final int width,
             final int height) throws Exception {
+
+        Image texture = new Image("D:/My/java/cg/Object-Viewer/3DModels/CaracalCube/caracal_texture.png"); //пока так, но должно быть так:
+       //Image texture1 = mesh.getTexture();
+
+        mesh.triangulatingPolygons = mesh.triangulateModel();
 
         if (mesh == null || mesh.triangulatingPolygons.isEmpty() || mesh.vertices.isEmpty()) {
             return; // Нечего отрисовывать
@@ -54,6 +63,7 @@ public class RenderEngWithTriangFill {
              */
             ArrayList<Vector2D> resultPoints = new ArrayList<>();
             for (int vertexInPolygonInd = 0; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
+                //идем по точкам полигона
                 Vector3D vertex = mesh.vertices.get(mesh.triangulatingPolygons.get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
                 Vector2D textVert = mesh.textureVertices.get(mesh.triangulatingPolygons.get(polygonInd).getTextureVertexIndices().get(vertexInPolygonInd));
 
@@ -68,13 +78,19 @@ public class RenderEngWithTriangFill {
                     continue; // Если w = 0, пропускаем эту вершину (вырождение)
                 }
                 Vector2D resultPoint = vertexToPoint(new Vector3D(new double[]{result.get(0), result.get(1), result.get(2)}), width, height);
-                arrX[vertexInPolygonInd] = (int)resultPoint.get(0);
-                arrY[vertexInPolygonInd] = (int)resultPoint.get(1);
+                arrX[vertexInPolygonInd] = (int) resultPoint.get(0);
+                arrY[vertexInPolygonInd] = (int) resultPoint.get(1);
                 textureVertices.add(textVert);
                 resultPoints.add(resultPoint);
-            }
 
-            for (int vertexInPolygonInd = 1; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
+
+            }
+            //здесь мы вызываем ту или иную растерезацию
+
+               //RasterezationOneColor.fillTriangle(graphicsContext, arrX, arrY, arrZ, Color.ORANGE, zBuffer);
+            Rasterization.fillTriangleWithTexture(graphicsContext, arrX,arrY,arrZ, textureVertices, texture, zBuffer);
+
+           /* for (int vertexInPolygonInd = 1; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
                 graphicsContext.strokeLine(
                         resultPoints.get(vertexInPolygonInd - 1).get(0),
                         resultPoints.get(vertexInPolygonInd - 1).get(1),
@@ -82,14 +98,21 @@ public class RenderEngWithTriangFill {
                         resultPoints.get(vertexInPolygonInd).get(1));
             }
 
+            */
+
             /*
             насколько я понимаю, вызов растеризации треугольника с текстурой, должен быть где-то здесь
              */
-            graphicsContext.strokeLine(
+
+            /*graphicsContext.strokeLine(
                     resultPoints.get(nVerticesInPolygon - 1).get(0),
                     resultPoints.get(nVerticesInPolygon - 1).get(1),
                     resultPoints.getFirst().get(0),
                     resultPoints.getFirst().get(1));
+
+             */
+
+
         }
     }
 }
