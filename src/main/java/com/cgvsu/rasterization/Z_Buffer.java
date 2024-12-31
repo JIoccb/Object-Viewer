28bd934d2646194921c8f810aa9daf32ec2172a1
Artyom Rashevsky <albatroz3000@yandex.ru>
2024-12-31 21:56:18 +0300

    refactor: move fields to the constructor

--- src/main/java/com/cgvsu/rasterization/Z_Buffer.java
+++ src/main/java/com/cgvsu/rasterization/Z_Buffer.java
@@ -3,14 +3,10 @@ package com.cgvsu.rasterization;
 import java.util.Arrays;
 
 public class Z_Buffer {
-    private int width;
-    private int height;
 
     private final double[][] zBuffer;
 
     public Z_Buffer(int width, int height) {
-        this.width = width;
-        this.height = height;
         zBuffer = new double[width][height];
 
         for (int i = 0; i < width; i++) {
