309411f56c923f3d4144ab3edea982b1bfd8aad2
Artyom Rashevsky <albatroz3000@yandex.ru>
2025-01-07 04:27:09 +0300

    style

--- src/main/java/com/cgvsu/math/Global.java
+++ src/main/java/com/cgvsu/math/Global.java
@@ -2,5 +2,5 @@ package com.cgvsu.math;
 
 public class Global {
     public static final double EPS = 1e-6;
-    public static final int MAX_COLOR_VALUE =  255;
+    public static final int MAX_COLOR_VALUE = 255;
 }
